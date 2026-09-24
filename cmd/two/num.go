639d9c80a27b6f673c26436effe134d00cde1965
Dima Polushkin <19polushkin.dima87@gmail.com>
2026-09-24 06:55:31 +0300

    add num float

--- cmd/two/num.go
+++ cmd/two/num.go
@@ -7,9 +7,13 @@ import (
 var x int = 10
 var y int = x * 2
 var i int = 5
+var a float64 = 4.4
+
+var t float64 = float64(x) + a
 
 func main() {
 	fmt.Println("y = ", y)
 	fmt.Println(y == i)
 	fmt.Println(y != i)
+	fmt.Println(t)
 }
